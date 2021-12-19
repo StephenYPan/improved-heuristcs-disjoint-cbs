133e38418d6dfe12b0b81defeb8b75da4ce63af3
anhd <anh@sfu.ca>
2021-12-19 01:00:30 -0800

    generate instancs for density 30

--- map_generator.py
+++ map_generator.py
@@ -10,6 +10,7 @@ from single_agent_planner import a_star, compute_heuristics
 ROAD = '.'
 WALL = '@'
 
+
 def map_init(width, height, density):
     map = []
     for i in range(0, height):
@@ -24,7 +25,7 @@ def map_init(width, height, density):
         y = loc[1]
         map[x][y] = True
         locs.remove(loc)
-    assert(num_walls+len(locs)==int(width*height))
+    assert(num_walls+len(locs) == int(width*height))
     return map, locs
 
 
@@ -32,7 +33,8 @@ def is_valid(map, start_loc, end_loc, agent):
     h_value = compute_heuristics(map, end_loc)
     try:
         # solver = IndependentSolver(map, start_loc, end_loc).find_solution()
-        solver = a_star(map, start_loc, end_loc, h_value, agent, constraints=[])
+        solver = a_star(map, start_loc, end_loc,
+                        h_value, agent, constraints=[])
     except BaseException:
         return False
     return True
@@ -43,7 +45,7 @@ def generate_loc(map, left_loc, n_agents):
     end_locs = []
     for agent in range(n_agents):
         agent_locs = random.sample(left_loc, 2)
-        while is_valid(map, agent_locs[0], agent_locs[1], agent)==False:
+        while is_valid(map, agent_locs[0], agent_locs[1], agent) == False:
             agent_locs = random.sample(left_loc, 2)
         left_loc.remove(agent_locs[0])
         left_loc.remove(agent_locs[1])
@@ -53,13 +55,18 @@ def generate_loc(map, left_loc, n_agents):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='custom instances for benchmarking')
-    parser.add_argument('--size', type=int, default=8, help='generate N x N size map')
-    parser.add_argument('--dense', type=float, default=0.2, help='the obstacles density of the map')
-    parser.add_argument('--agent', type=int, default=4, help='number of agents')
-    parser.add_argument('--num', type=int, default=50, help='number of instances needed')
+    parser = argparse.ArgumentParser(
+        description='custom instances for benchmarking')
+    parser.add_argument('--size', type=int, default=8,
+                        help='generate N x N size map')
+    parser.add_argument('--dense', type=float, default=0.2,
+                        help='the obstacles density of the map')
+    parser.add_argument('--agent', type=int, default=4,
+                        help='number of agents')
+    parser.add_argument('--num', type=int, default=50,
+                        help='number of instances needed')
     args = parser.parse_args()
-    path = os.path.join(os.getcwd(), "./custom_instances/{}_agents_{}_density".format(args.agent, int(args.dense*100)))
+    path = os.path.join(os.getcwd(), "./additional_instances")
     Path(path).mkdir(parents=True, exist_ok=True)
     for num in range(1, args.num+1):
         random.seed(datetime.now())
@@ -73,7 +80,7 @@ if __name__ == "__main__":
         #         start_locs, end_locs = generate_loc(left_loc, args.agent)
         #     else:
         #         break
-        with open(os.path.join(path, 'test_{}.txt'.format(num)), 'w') as f:
+        with open(os.path.join(path, '{}_agents_{}_density_test_{}.txt'.format(args.agent, args.dense, num)), 'w') as f:
             f.write("{} {}\n".format(args.size, args.size))
             for i in range(len(map)):
                 for j in range(len(map[i])):
